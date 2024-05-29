af24d567ce2df544232081b847349a6e27b69938
Timothy jan <jan.timothy@gmail.com>
2024-05-29 09:31:42 -0700

    Review Service initial commit

--- frontend/src/app/services/review.service.ts
+++ frontend/src/app/services/review.service.ts
@@ -1,45 +1,47 @@
 import { Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Review } from '../models/review';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewService {
   private url = 'http://localhost:5200';
-  employees$ = signal<Review[]>([]);
-  employee$ = signal<Review>({} as Review);
+  reviews$ = signal<Review[]>([]);
+  review$ = signal<Review>({} as Review);
 
   constructor(private httpClient: HttpClient) { }
 
   private refreshReviews() {
-    this.httpClient.get<Review[]>(`${this.url}/employees`)
-      .subscribe(employees => {
-        this.employees$.set(employees);
+    this.httpClient.get<Review[]>(`${this.url}/reviews`)
+      .subscribe(reviews => {
+        this.reviews$.set(reviews);
       });
   }
 
   getReviews() {
     this.refreshReviews();
-    return this.employees$();
+    return this.reviews$();
   }
 
   getReview(id: string) {
-    this.httpClient.get<Review>(`${this.url}/employees/${id}`).subscribe(employee => {
-      this.employee$.set(employee);
-      return this.employee$();
+    this.httpClient.get<Review>(`${this.url}/reviews/${id}`)
+    .subscribe(review => {
+      this.review$.set(review);
+      return this.review$();
     });
   }
 
-  createReview(employee: Review) {
-    return this.httpClient.post(`${this.url}/employees`, employee, { responseType: 'text' });
+  createReview(review: Review) {
+    return this.httpClient.post(`${this.url}/reviews`, review, { responseType: 'text' });
   }
 
-  updateReview(id: string, employee: Review) {
-    return this.httpClient.put(`${this.url}/employees/${id}`, employee, { responseType: 'text' });
+  updateReview(id: string, review: Review) {
+    return this.httpClient.put(`${this.url}/reviews/${id}`, review, { responseType: 'text' });
   }
 
   deleteReview(id: string) {
-    return this.httpClient.delete(`${this.url}/employees/${id}`, { responseType: 'text' });
+    return this.httpClient.delete(`${this.url}/reviews/${id}`, { responseType: 'text' });
   }
 }
