b363ee4e63239331dda3e40978f3e8dd5825c641
Timothy jan <jan.timothy@gmail.com>
2024-05-29 09:30:26 -0700

    Enable HTTP

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideAnimations, provideNoopAnimations } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
