473aaa0aed18dd3e4adfba0ccddeeb0520ae1c14
Timothy jan <jan.timothy@gmail.com>
2024-06-04 13:40:02 -0700

    Review Database created

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -40,11 +40,11 @@ async function applySchemaValidation(db: mongodb.Db) {
 
   // Try applying the modification to the collection, if the collection doesn't exist, create it
    await db.command({
-        collMod: "employees",
+        collMod: "reviews",
         validator: jsonSchema
     }).catch(async (error: mongodb.MongoServerError) => {
         if (error.codeName === "NamespaceNotFound") {
-            await db.createCollection("employees", {validator: jsonSchema});
+            await db.createCollection("reviews", {validator: jsonSchema});
         }
     });
 }
